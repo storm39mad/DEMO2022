ebfd706efa7d43749a744ec75ab4a0c546f6686f
storm39mad <79700810+storm39mad@users.noreply.github.com>
2022-02-08 18:08:24 +0300

    Update output.tf

--- azure/autodeploy/output.tf
+++ azure/autodeploy/output.tf
@@ -1,3 +1,31 @@
+data "azurerm_public_ip" "isp-public-ip" {
+  name                = "isp-public-ip"
+  resource_group_name = data.azurerm_resource_group.RGKP11.name
+  depends_on = [
+    azurerm_linux_virtual_machine.ISP
+  ]
+}
+output "isp-public-ip" {
+  value = data.azurerm_public_ip.isp-public-ip.ip_address
+}
+data "azurerm_public_ip" "rtr-l-public-ip" {
+  name                = "rtr-l-public-ip"
+  resource_group_name = data.azurerm_resource_group.RGKP11.name
+  depends_on = [
+    azurerm_linux_virtual_machine.RTR-L
+  ]
+}
 output "rtr-l-public-ip" {
-  value = azurerm_linux_virtual_machine.RTR-L.public_ip_address
+  value = data.azurerm_public_ip.rtr-l-public-ip.ip_address
+}
+
+data "azurerm_public_ip" "rtr-r-public-ip" {
+  name                = "rtr-r-public-ip"
+  resource_group_name = data.azurerm_resource_group.RGKP11.name
+  depends_on = [
+    azurerm_linux_virtual_machine.RTR-R
+  ]
+}
+output "rtr-r-public-ip" {
+  value = data.azurerm_public_ip.rtr-r-public-ip.ip_address
 }
